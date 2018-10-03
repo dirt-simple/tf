8c1b3276499bee37320b3cbeed2132be5ff6789b
Brian Jones <beeceej.code@gmail.com>
2018-10-03 00:10:12 -0500

    terraform fmt

--- cloudflare/single_worker/example/main.tf
+++ cloudflare/single_worker/example/main.tf
@@ -3,14 +3,14 @@ terraform {
     # replace with your bucket/region/key
     bucket = "beeceej-ops"
     region = "us-east-1"
-    key = "tfstate/infrastructure/cloudflare_single_worker_example/terraform.tfstate"
+    key    = "tfstate/infrastructure/cloudflare_single_worker_example/terraform.tfstate"
   }
 }
 
 module "single_worker" {
-    source = "../"
-    enabled_route_patterns = ["blog.beeceej.com/*"]
-    disabled_route_patterns = ["blog.beeceej.com/"]
-    account_zone = "beeceej.com"
-    script_path = "worker/index.js"
+  source                  = "../"
+  enabled_route_patterns  = ["blog.beeceej.com/*"]
+  disabled_route_patterns = ["blog.beeceej.com/"]
+  account_zone            = "beeceej.com"
+  script_path             = "worker/index.js"
 }
